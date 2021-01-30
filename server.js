1edf1c9b1989d3df2424aa244de1226cf3e2e1ee
Kyle <kjbowman00@vt.edu>
2021-01-30 13:39:30 -0500

    Some basic server side stuff

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
+//Player stuff
+//EX player: {position: {x: 12, room: 1}, name: "Bob", avatar: 3}
+var players = new Map();
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
@@ -11,6 +18,30 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        let p = players.get(socket.id);
+        if (p != undefined) {
+            players.delete(socket.id);
+            console.log(p.name + " has left the game!");
+        }
+    });
+
+    socket.on('join', (info) => {
+        console.log(info.name + " has joined the game as avatar " + info.avatar);
+
+        let p = {
+            position: { x: 5, room: 1 },
+            name: info.name,
+            avatar: info.avatar
+        };
+
+        players.set(socket.id, p);
+    });
+
+    socket.on('update_pos', (position) => {
+    });
+
+    socket.on('chat_send', (message) => {
+        console.log(message);
     });
 });
 
