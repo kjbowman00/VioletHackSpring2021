bb4ed2be3e8a78ccc693d416c1f34c053e808c8e
klaravw <49321480+klaravw@users.noreply.github.com>
2021-01-30 22:08:40 -0500

    made avatars change

---
+++ public/avatarChanger.js
@@ -0,0 +1,18 @@
+"use strict";
+var avatarNum = 1;
+
+function changeAvatar(dir){
+    if(dir<0){
+        avatarNum = avatarNum - 1;
+        if (avatarNum==0){
+            avatarNum = 6;
+        }
+    }
+    if(dir>0){
+        avatarNum = avatarNum + 1;
+        if (avatarNum==7){
+            avatarNum = 1;
+        }
+    }
+    document.getElementById("avatar").src = "/Avatars/avatar"+avatarNum+".png";
+}
